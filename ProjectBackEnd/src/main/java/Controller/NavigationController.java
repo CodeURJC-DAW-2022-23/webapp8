fa2ef6257095eab061411c2ebcaafb323a126e19
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2023-02-22 14:40:18 +0100

    Add navigation with Spring
    Now you can navigate through the web using Spring

--- ProjectBackEnd/src/main/java/Controller/NavigationController.java
+++ ProjectBackEnd/src/main/java/Controller/NavigationController.java
@@ -10,7 +10,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class NavigationController {
 
     @GetMapping("/home")
-    public String bruh() {
+    public String toHome() {
         return "home";
+    }
+
+    @GetMapping("/explore")
+    public String toExplore() {
+        return "explore";
+    }
+
+    @GetMapping("/notifications")
+    public String toNotifications() {
+        return "notifications";
+    }
+
+    @GetMapping("/bookmarks")
+    public String toBookmark() {
+        return "bookmarks";
+    }
+
+    @GetMapping("/profile")
+    public String toProfile() {
+        return "profile";
+    }
+
+    @GetMapping("/write-tweet")
+    public String toWriteTweet() {
+        return "write-tweet";
+    }
+
+    @GetMapping("/error")
+    public String toError() {
+        return "error";
     }
 }
