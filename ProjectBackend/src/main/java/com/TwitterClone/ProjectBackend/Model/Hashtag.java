3b1111c17d2ed32e63b8b5efe353b4a1a93b2744
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2023-02-24 10:53:18 +0100

    Update application.properties
    Now the console will show the SQL queries

--- ProjectBackend/src/main/java/com/TwitterClone/ProjectBackend/Model/Hashtag.java
+++ ProjectBackend/src/main/java/com/TwitterClone/ProjectBackend/Model/Hashtag.java
@@ -1,5 +1,8 @@
 package com.TwitterClone.ProjectBackend.Model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -7,11 +10,13 @@ import java.util.UUID;
 /**
  * Stores all tweets that contain this hashtag
  */
+@Getter
+@Setter
 public class Hashtag {
 
     private String name;
 
-    private List<UUID> tweetList;
+    private List<Long> tweetList;
 
     public Hashtag(String name) {
         this.name = name;
