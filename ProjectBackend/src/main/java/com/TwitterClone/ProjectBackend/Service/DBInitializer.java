433d4809d14bf54be862482adbf62a0adffa6b6f
MiguelAngelSM <ma.sanchezm.2021@alumnos.urjc.es>
2023-02-27 01:52:30 +0100

    Add more sample data to DB
    It just need to add Trend and Hashtag

--- ProjectBackend/src/main/java/com/TwitterClone/ProjectBackend/Service/DBInitializer.java
+++ ProjectBackend/src/main/java/com/TwitterClone/ProjectBackend/Service/DBInitializer.java
@@ -1,6 +1,8 @@
 package com.TwitterClone.ProjectBackend.Service;
 
+import com.TwitterClone.ProjectBackend.Model.Notification;
 import com.TwitterClone.ProjectBackend.Model.Tweet;
+import com.TwitterClone.ProjectBackend.Repository.NotificationRepository;
 import com.TwitterClone.ProjectBackend.Repository.TweetRepository;
 import com.TwitterClone.ProjectBackend.Repository.UserRepository;
 import com.TwitterClone.ProjectBackend.userManagement.User;
@@ -13,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.sql.Blob;
@@ -30,6 +33,9 @@ public class DBInitializer {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private NotificationRepository notificationRepository;
+
     @Value("${admin.pass}")
     String adminPass;
 
@@ -38,23 +44,36 @@ public class DBInitializer {
 
         //Sample Users
         String [] files = {"example_data/elrubius_profilepic.jpg","example_data/elrubius_profilebanner.jpg"};
-        User user1 = new User("@Rubiu5","elrubius","Rata Noruega.  Me gustan los gatos obesos.","rubius@gmail.com","rubius",files, LocalDate.of(2013,10,13),"VERIFIED");
+        User user1 = new User("@Rubiu5","elrubius","Rata Noruega.  Me gustan los gatos obesos.","rubius@gmail.com",passwordEncoder.encode("rubius"),files, LocalDate.of(2013,10,13),"VERIFIED");
         files = new String[]{"example_data/KOI_KEYLAND_profilepic.jpg", "example_data/KOI_KEYLAND_profilebanner.jpg"};
-        User user2 = new User("@Keyland71", "KOI KEYLAND71", "19 y/o\nRocket League proplayer for @KOI", "example2@gmail.com", "examplePassword2",files, LocalDate.of(2018,4,21), "USER");
+        User user2 = new User("@Keyland71", "KOI KEYLAND71", "19 y/o\nRocket League proplayer for @KOI", "example2@gmail.com", passwordEncoder.encode("examplePassword2"),files, LocalDate.of(2018,4,21), "PUBLIC");
         files = new String[]{"example_data/Alanis_profilepic.jpg", "example_data/Alanis_profilebanner.jpg"};
-        User user3 = new User("@antonioalanxs", "Alanís",  "",  "example3@gmail.com", "examplePassword3",files, LocalDate.of(2019,8,7), "PRIVATE");
+        User user3 = new User("@antonioalanxs", "Alanís",  "",  "example3@gmail.com", passwordEncoder.encode("examplePassword3"),files, LocalDate.of(2019,8,7), "PRIVATE");
         files = new String[]{"example_data/Ibai_profilepic.jpg", "example_data/Ibai_profilebanner.jpg"};
-        User user4 = new User("@ibai","Ibai","Sigue a nuestros equipos @KOI y @PorcinosFC, http://twitch.tv/ibai","ibai@gmail.com","ibai",files, LocalDate.of(2014,8,5), "VERIFIED");
+        User user4 = new User("@ibai","Ibai","Sigue a nuestros equipos @KOI y @PorcinosFC, http://twitch.tv/ibai","ibai@gmail.com",passwordEncoder.encode("ibai"),files, LocalDate.of(2014,8,5), "VERIFIED");
         userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+        userRepository.save(user4);
+        List<User> u = userRepository.findAll();
+        u.get(3).addFollower(u.get(0));
+        u.get(0).addFollowed(u.get(3));
+        u.get(2).addFollower(u.get(1));
+        u.get(1).addFollowed(u.get(2));
+        u.get(1).addFollower(u.get(3));
+        u.get(3).addFollowed(u.get(1));
+        u.get(2).addFollower(u.get(0));
+        u.get(0).addFollowed(u.get(2));
+        userRepository.save(u.get(0));
+        userRepository.save(u.get(1));
+        userRepository.save(u.get(2));
+        userRepository.save(u.get(3));
 
         User testUser = new User("user", passwordEncoder.encode("pass"),"user@mail.com", "USER");
         userRepository.save(testUser);
 
         User admin = new User("admin", adminPass, "admin@gmail.com", "ADMIN");
         userRepository.save(admin);
-        userRepository.save(user2);
-        userRepository.save(user3);
-        userRepository.save(user4);
 
         List<User> users = userRepository.findAll();
         //Sample Tweets
@@ -151,7 +170,7 @@ public class DBInitializer {
                 null
         };
         Tweet tweet7 = new Tweet("En primaria deberían poner una asignatura de coger aceitunas @Keyland71", users.get(2), LocalDateTime.of(2023,02,20,14,03,00), null, images);
-
+        //Comments
         images = new Blob[] {
                 null,
                 null,
@@ -161,45 +180,64 @@ public class DBInitializer {
         Tweet tweet8 = new Tweet("Tienes razón \uD83E\uDD75\uD83E\uDD75", users.get(2), LocalDateTime.of(2023,02,20,14,04,00), null, images);
         tweetRepository.save(tweet7);
         tweetRepository.save(tweet8);
+        tweets = tweetRepository.findAll();
+        tweets.get(tweets.size()-2).addComment(tweets.get(tweets.size()-1));
+        tweets.get(tweets.size()-2).addComment(tweets.get(tweets.size()-3));
+        tweetRepository.save(tweets.get(tweets.size()-2));
 
-        /*tweets = tweetRepository.findAll();
-        users.get(3).addTweet(tweets.get(0));
-        users.get(3).addTweet(tweets.get(1));
-        users.get(3).addTweet(tweets.get(2));
-        users.get(0).addTweet(tweets.get(3));
-        users.get(0).addTweet(tweets.get(4));
-        users.get(1).addTweet(tweets.get(5));
-        users.get(1).addTweet(tweets.get(6));
-        users.get(1).addTweet(tweets.get(7));
-        users.get(2).addTweet(tweets.get(9));
-        users.get(2).addTweet(tweets.get(9));
+        //Sample notifications
+        tweets = tweetRepository.findAll();
+        users = userRepository.findAll();
+
+        Tweet tweet = tweets.get(5);
+        User user = users.get(2);
+        Notification notification = new Notification(tweet,user,tweet.getUser(),LocalDateTime.of(2022,06,14,17,0,0),"LIKE");
+        notificationRepository.save(notification);
+
+        tweet = tweets.get(8);
+        user = users.get(3);
+        notification = new Notification(tweet,tweet.getUser(),user,LocalDateTime.of(2023,02,20,14,3,0),"RETWEET");
+        notificationRepository.save(notification);
+
+        tweet = tweets.get(7);
+        user = users.get(2);
+        notification = new Notification(tweet,tweet.getUser(),user,LocalDateTime.of(2023,02,20,14,9,0),"COMMENT");
+        notificationRepository.save(notification);
+
+        tweet = tweets.get(3);
+        user = users.get(3);
+        notification = new Notification(tweet,tweet.getUser(),user,LocalDateTime.of(2023,02,20,14,9,0),"CITATION");
+        notificationRepository.save(notification);
+
+        User user_notificated = users.get(1);
+        user = users.get(3);
+        notification = new Notification(tweet,user_notificated,user,LocalDateTime.of(2020,04,11,15,3,0),"FOLLOW");
+        notificationRepository.save(notification);
+
+        user_notificated = users.get(2);
+        user = users.get(1);
+        notification = new Notification(tweet,user_notificated,user,LocalDateTime.of(2020,04,11,15,3,0),"FOLLOW");
+        notificationRepository.save(notification);
+
+        user_notificated = users.get(3);
+        user = users.get(0);
+        notification = new Notification(tweet,user_notificated,user,LocalDateTime.of(2020,04,11,15,3,0),"FOLLOW");
+        notificationRepository.save(notification);
+
+        user_notificated = users.get(0);
+        user = users.get(2);
+        notification = new Notification(tweet,user_notificated,user,LocalDateTime.of(2020,04,11,15,3,0),"FOLLOW");
+        notificationRepository.save(notification);
+
+        //Bookmarks
+        users.get(0).addBookmark(tweets.get(0));
         userRepository.save(users.get(0));
-        userRepository.save(users.get(1));
-        userRepository.save(users.get(2));
-        userRepository.save(users.get(3));*/
+
+        //Interactions
+        tweets.get(5).addLike(users.get(2));
+        tweets.get(8).addRetweet(users.get(2));
+        tweets.get(8).addRetweet(users.get(3));
+        tweetRepository.save(tweets.get(5));
+        tweetRepository.save(tweets.get(8));
     }
 }
-
-/*    @Autowired
-    private PasswordEncoder passwordEncoder;*/
-
-    /*@PostConstruct
-    public void init() throws IOException, URISyntaxException {
-
-        //Sample Users
-        String [] files = new String[]{"example_data/KOI_KEYLAND_profilepic.jpg", "example_data/KOI_KEYLAND_profilebanner.jpg"};
-        User user2 = new User("@Keyland71", "KOI KEYLAND71", "19 y/o\nRocket League proplayer for @KOI", "example2@gmail.com", "examplePassword2",files, LocalDate.of(2018,4,21), "USER");
-        userRepository.save(user2);
-
-        User testUser = new User("user", passwordEncoder.encode("pass"),"user@mail.com");
-        userRepository.save(testUser);
-
-        //Sample Tweets
-        Tweet tweet1 = new Tweet("Fotito con mi fan \uD83D\uDCAA\uD83C\uDFFD\uD83D\uDCAA\uD83C\uDFFD\uD83D\uDD25\uD83D\uDD25", user2, LocalDateTime.of(2022,06,14,16,03,00), null);
-        Tweet tweet2 = new Tweet("MarvelsSpiderManMilesMorales", user2, LocalDateTime.of(2021,8,16,20,00,00), null);
-
-        tweetRepository.save(tweet1);
-        tweetRepository.save(tweet2);
-
-    }
-}*/
