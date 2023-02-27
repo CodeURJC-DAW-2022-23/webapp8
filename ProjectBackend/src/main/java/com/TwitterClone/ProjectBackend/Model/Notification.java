457bb1f90d3db5bef2b794071e385d3952baa64b
MiguelAngelSM <ma.sanchezm.2021@alumnos.urjc.es>
2023-02-27 01:51:12 +0100

    Add Notification to DB

--- ProjectBackend/src/main/java/com/TwitterClone/ProjectBackend/Model/Notification.java
+++ ProjectBackend/src/main/java/com/TwitterClone/ProjectBackend/Model/Notification.java
@@ -2,24 +2,63 @@ package com.TwitterClone.ProjectBackend.Model;
 
 import com.TwitterClone.ProjectBackend.userManagement.User;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.UUID;
 
 /**
  * This class contains all the information related to a notification
  */
+@Entity
 public class Notification {
-    private final UUID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private final Long id;
+    @ManyToOne
     private Tweet tweetTrigger;
+    @ManyToOne
     private User userToNotify;
+    @ManyToOne
     private User userWhoNotifies;
     private LocalDateTime date;
-    private NotificationType type;
+    private String type;
 
-
+    /**
+     * Deafult constructor
+     */
     public Notification() {
-        id = UUID.randomUUID();
-        date =LocalDateTime.now();
-        //Falta inicializar el resto
+        id = new Random().nextLong();
+    }
+
+    /**
+     * Constructor
+     * @param tweet
+     * @param userToNotify
+     * @param userWhoNotifies
+     */
+    public Notification(Tweet tweet, User userToNotify, User userWhoNotifies, String type) {
+        id = new Random().nextLong();
+        this.userToNotify = userToNotify;
+        this.userWhoNotifies = userWhoNotifies;
+        this.tweetTrigger = tweet;
+        this.date = LocalDateTime.now();
+        this.type = type;
+    }
+
+    /**
+     * This constructor is for sample data
+     * @param tweet
+     * @param userToNotify
+     * @param userWhoNotifies
+     * @param date
+     */
+    public Notification(Tweet tweet, User userToNotify, User userWhoNotifies, LocalDateTime date, String type) {
+        id = new Random().nextLong();
+        this.userToNotify = userToNotify;
+        this.userWhoNotifies = userWhoNotifies;
+        this.tweetTrigger = tweet;
+        this.date = date;
+        this.type = type;
     }
 }
