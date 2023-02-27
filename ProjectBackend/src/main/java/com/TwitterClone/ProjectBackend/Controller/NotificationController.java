66fd753914595b030e2de6d2425fb1fa546dc903
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2023-02-27 16:19:58 +0100

    Delete Duplicated ProjectBackend

--- ProjectBackend/src/main/java/com/TwitterClone/ProjectBackend/Controller/NotificationController.java
+++
@@ -1,7 +0,0 @@
-package com.TwitterClone.ProjectBackend.Controller;
-
-/**
- * This class is on charge of controlling all request from the view regarding the notifications
- */
-public class NotificationController {
-}
