086bab5aec23c3cb688d4c569fc1558623645984
Camyus <lm.camino.2020@alumnos.urjc.es>
2023-03-15 18:05:14 +0100

    Add LoginRestController and Service

---
+++ ProjectBackEnd/src/main/java/com/TwitterClone/ProjectBackend/RestController/LoginRestController.java
@@ -0,0 +1,41 @@
+package com.TwitterClone.ProjectBackend.RestController;
+
+import com.TwitterClone.ProjectBackend.Security.jwt.AuthResponse;
+import com.TwitterClone.ProjectBackend.Security.jwt.LoginRequest;
+import com.TwitterClone.ProjectBackend.Security.jwt.UserLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/api")
+public class LoginRestController {
+
+    @Autowired
+    private UserLoginService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @CookieValue(name = "accessToken", required = false) String accessToken,
+            @CookieValue(name = "refreshToken", required = false) String refreshToken,
+            @RequestBody LoginRequest loginRequest) {
+
+        return userService.login(loginRequest, accessToken, refreshToken);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refreshToken(
+            @CookieValue(name = "refreshToken", required = false) String refreshToken) {
+
+        return userService.refresh(refreshToken);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response) {
+
+        return ResponseEntity.ok(new AuthResponse(AuthResponse.Status.SUCCESS, userService.logout(request, response)));
+    }
+}
