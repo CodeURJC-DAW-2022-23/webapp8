d531fdaf8ae13b0f3e4b6204fd068191ca6528de
Camyus <lm.camino.2020@alumnos.urjc.es>
2023-03-15 18:05:14 +0100

    Add LoginRestController

--- ProjectBackEnd/src/main/java/com/TwitterClone/ProjectBackend/Security/RestSecurityConfig.java
+++ ProjectBackEnd/src/main/java/com/TwitterClone/ProjectBackend/Security/RestSecurityConfig.java
@@ -48,9 +48,9 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
         http.antMatcher("/api/**");
 
         // URLs that need authentication to access to it
-        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/books/**").hasRole("USER");
-        http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/books/**").hasRole("USER");
-        http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/books/**").hasRole("ADMIN");
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/tweets/write-tweet").hasRole("USER");
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/tweets/reply-tweet/**").hasRole("USER");
+        http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/tweets/**").hasRole("ADMIN");
 
         // Other URLs can be accessed without authentication
         http.authorizeRequests().anyRequest().permitAll();
