25b3ace3b9ae6b0d9dd027a421dcbc3da0651f5a
Camyus <lm.camino.2020@alumnos.urjc.es>
2023-03-18 13:47:21 +0100

    Add UserSignUpRestController
    The new controller and its documentation has been added.
    The LogInRestController documentation have been updated

--- ProjectBackEnd/src/main/java/com/TwitterClone/ProjectBackend/Controller/RestController/LoginRestController.java
+++ ProjectBackEnd/src/main/java/com/TwitterClone/ProjectBackend/Controller/RestController/LoginRestController.java
@@ -3,6 +3,11 @@ package com.TwitterClone.ProjectBackend.Controller.RestController;
 import com.TwitterClone.ProjectBackend.Security.jwt.AuthResponse;
 import com.TwitterClone.ProjectBackend.Security.jwt.LoginRequest;
 import com.TwitterClone.ProjectBackend.Security.jwt.UserLoginService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +22,13 @@ public class LoginRestController {
     @Autowired
     private UserLoginService userService;
 
+    @Operation(summary = "Check the credentials of a user and let them in if they are correct")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Log In successful", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Object.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "invalid credentials", content = @Content)
+    })
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(
             @CookieValue(name = "accessToken", required = false) String accessToken,
@@ -25,7 +37,13 @@ public class LoginRestController {
 
         return userService.login(loginRequest, accessToken, refreshToken);
     }
-
+    @Operation(summary = "Refresh the content of a page")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Page has been successfully refreshed", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Object.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "UURL not found", content = @Content)
+    })
     @PostMapping("/refresh")
     public ResponseEntity<AuthResponse> refreshToken(
             @CookieValue(name = "refreshToken", required = false) String refreshToken) {
@@ -33,6 +51,13 @@ public class LoginRestController {
         return userService.refresh(refreshToken);
     }
 
+    @Operation(summary = "Log a user out of the application")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Log Out successful", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Object.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "invalid request", content = @Content)
+    })
     @PostMapping("/logout")
     public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response) {
 
---
+++ ProjectBackEnd/src/main/java/com/TwitterClone/ProjectBackend/Controller/RestController/UserSignUpRestController.java
@@ -0,0 +1,64 @@
+package com.TwitterClone.ProjectBackend.Controller.RestController;
+
+import com.TwitterClone.ProjectBackend.DTO.RegisteredRequest;
+import com.TwitterClone.ProjectBackend.Model.MustacheObjects.TweetInformation;
+import com.TwitterClone.ProjectBackend.Model.Tweet;
+import com.TwitterClone.ProjectBackend.userManagement.User;
+import com.TwitterClone.ProjectBackend.userManagement.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+public class UserSignUpRestController {
+    @Autowired
+    private UserService service;
+
+    @Operation(summary = "Redirect the user to the signUp view")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Registration successful", content = {
+                    @Content(mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "400", description = "Invalid rquest", content = @Content)
+    })
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup (@RequestBody RegisteredRequest request)
+            throws MessagingException, IOException {
+        service.signup(request);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "Check the verification code to create an account")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Account verified", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Object.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "invalid Token", content = @Content)
+    })
+    @GetMapping("/verify")
+    public ResponseEntity<Object> verifyUser(@Param("code") String code) {
+
+        if (service.verify(code)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return ResponseEntity.accepted().build();
+    }
+
+}
