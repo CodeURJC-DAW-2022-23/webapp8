4a8c887a982200a63c1a68f0da1f430791d65a19
Sergio de Oro Fernández <sergiodeoro422003@gmail.com>
2023-04-16 23:48:36 +0200

    Add notification service

---
+++ ProjectFrontEnd/src/app/services/notification.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, catchError, throwError } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class NotificationService {
+    constructor(
+        private httpClient: HttpClient,
+    ){}
+
+    getSomeNotifications(from: number, size: number): Observable<Notification[]> {
+        let url = "/api/notifications?from=" + from + "&size=" + size;
+        return this.httpClient.get(url).pipe(
+			catchError(error => this.handleError(error))
+		) as Observable<Notification[]>;
+    }
+
+    getSomeMentions(from: number, size: number): Observable<Notification[]> {
+        let url = "/api/mentions?from=" + from + "&size=" + size;
+        return this.httpClient.get(url).pipe(
+			catchError(error => this.handleError(error))
+		) as Observable<Notification[]>;
+    }
+
+    private handleError(error: any) {
+		console.error(error);
+		return throwError("Server error (" + error.status + "): " + error.text())
+	}
+}
