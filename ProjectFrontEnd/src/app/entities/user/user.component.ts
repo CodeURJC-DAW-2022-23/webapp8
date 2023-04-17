69ff3d1ec7c3ba7b81c58afe9975ae7fdb7eded4
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2023-04-17 11:28:57 +0200

    Fix problem with User component

--- ProjectFrontEnd/src/app/entities/user/user.component.ts
+++ ProjectFrontEnd/src/app/entities/user/user.component.ts
@@ -9,8 +9,7 @@ import { UserInformation } from '../user/user.model';
 export class UserComponent{
   @Input()
   userInformation: UserInformation;
-  @Input()
-  isLeftBar: boolean;
+
 
   srcImg: string;
   nickname: string;
@@ -32,7 +31,7 @@ export class UserComponent{
         <g>
             <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/>
         </g>
-    
+
     </svg>
     `
 }
