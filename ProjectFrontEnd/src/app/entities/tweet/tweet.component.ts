7b4299fcbfaf76fc6e53def5bb5409569b0a7f61
MiguelAngelSM <ma.sanchezm.2021@alumnos.urjc.es>
2023-04-18 10:45:16 +0200

    Add user svg to tweet component

--- ProjectFrontEnd/src/app/entities/tweet/tweet.component.ts
+++ ProjectFrontEnd/src/app/entities/tweet/tweet.component.ts
@@ -27,6 +27,19 @@ export class TweetComponent {
   isMedia4:boolean;
   threeImages: boolean;
 
+  svg:string;
+  userTypeSVGMap = {
+    "VERIFIED":"M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z",
+    "BANNED":"M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
+  };
+
+  viewBoxMap = {
+    "VERIFIED": "0 0 24 24",
+    "BANNED": "0 0 512 512",
+  }
+
+  viewBox:string;
+
   imageClass:string = "max-h-[500px] border bg-gray-4 dark:bg-twitter-dark-gray border-gray-4 rounded-xl overflow-hidden";
 
   ngOnInit(): void{
@@ -42,12 +55,15 @@ export class TweetComponent {
     this.tweet.urlToMedia4 = "/api/" + this.tweet.urlToMedia4
     this.threeImages = this.isMedia3 && !this.isMedia4;
     if (this.isMedia2){
-      this.imageClass += "grid grid-cols-2";
+      this.imageClass += " grid grid-cols-2";
     }
     if (this.isMedia4){
-      this.imageClass += "grid grid-rows-2";
+      this.imageClass += " grid grid-rows-2";
     }
+    this.svg = this.userTypeSVGMap[this.tweet.tweet.user.type]
+    this.viewBox = this.viewBoxMap[this.tweet.tweet.user.type]
   }
+
   giveRetweet(){
     if (! this.isLogged){
       return
@@ -61,6 +77,7 @@ export class TweetComponent {
     );
 
   }
+
   giveLike(){
     if (! this.isLogged){
       return
@@ -73,6 +90,7 @@ export class TweetComponent {
       error => console.error(error)
     );
   }
+
   giveBookmark(){
     if (! this.isLogged){
       return
@@ -94,4 +112,5 @@ export class TweetComponent {
             )
         }
   }
+
 }
