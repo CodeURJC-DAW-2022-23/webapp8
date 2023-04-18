ad9e13a9be588ed08a6f7cea33eece35ef5f6f4b
MiguelAngelSM <ma.sanchezm.2021@alumnos.urjc.es>
2023-04-18 16:09:00 +0200

    Fix spinner to the top

--- ProjectFrontEnd/src/app/entities/notification/notification-list/notification-list.component.ts
+++ ProjectFrontEnd/src/app/entities/notification/notification-list/notification-list.component.ts
@@ -66,7 +66,7 @@ export class NotificationListComponent {
   }
 
   addSpinner() {
-    document.getElementById("spinner").innerHTML = `<div class="flex items-center justify-center sticky pt-5">
+    document.getElementById("spinner").innerHTML = `<div class="flex items-center justify-center sticky">
                 <div class="fixed inline-block h-8 w-8 animate-spin rounded-full border-4 border-primary border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
                     <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
                         Loading...
