251d686dd856b2cd66890bdf196fe5cf65a0225f
Antonio José Alanís Bernal <antoniojalanis7131@gmail.com>
2023-04-19 17:37:35 +0200

    Refactor Left bar component

--- ProjectFrontEnd/src/app/commons/left-bar/left-bar.component.ts
+++ ProjectFrontEnd/src/app/commons/left-bar/left-bar.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { User } from 'src/app/entities/user/user.model';
-import { LoginService } from 'src/app/services/login.service';
+
 import { UserService } from 'src/app/services/user.service';
+import { LoginService } from 'src/app/services/login.service';
+
+import { User } from 'src/app/entities/user/user.model';
 
 @Component({
   selector: 'app-left-bar',
   templateUrl: './left-bar.component.html',
-  styleUrls: ['./left-bar.component.css']
+  styleUrls: ['./left-bar.component.css'],
+  providers: [UserService, LoginService]
 })
 export class LeftBarComponent {
 
-  nickname: String;
-  username: String;
-  urlToProfilePicture: String;
+  public nickname: string;
+  public username: string;
+  public urlToProfilePicture: string;
 
-  constructor(private router: Router, private userService: UserService, private loginService: LoginService) {
-    this.userService.getCurrentUser().subscribe(userInformation => {
+  constructor(private _userService: UserService, 
+    private _loginService: LoginService) {
+    this._userService.getCurrentUser().subscribe(userInformation => {
       this.urlToProfilePicture = "/api/" + userInformation.urlToProfilePic
 
       let user: User;
@@ -28,6 +31,6 @@ export class LeftBarComponent {
   }
 
   logOut() {
-    this.loginService.logOut();
+    this._loginService.logOut();
   }
 }
