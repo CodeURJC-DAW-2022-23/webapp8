6e902904d6c737f147728cf8703266627591a2f2
MiguelAngelSM <ma.sanchezm.2021@alumnos.urjc.es>
2023-04-22 00:02:35 +0200

    Add user component isExplorePage condition

--- ProjectFrontEnd/src/app/entities/user/user.component.ts
+++ ProjectFrontEnd/src/app/entities/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UserInformation } from '../user/user.model';
 import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -22,6 +23,8 @@ export class UserComponent {
   idSVG: string;
   buttonClass: boolean;
 
+  isExplorePage:boolean;
+
   svg:string;
   userTypeSVGMap = {
     "VERIFIED":"M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z",
@@ -35,14 +38,15 @@ export class UserComponent {
 
   viewBox:string;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
     this.srcImg = "api/" + this.userInformation.urlToProfilePic;
     this.nickname = this.userInformation.user.nickname;
     this.username = this.userInformation.user.username;
     this.idSVG = this.username + "-SVG";
-    this.blue = this.typeButton === "Verify"
+    this.blue = this.typeButton === "Verify";
+    this.isExplorePage = this.router.url.includes("explore");
   }
 
   loadSVG() {
