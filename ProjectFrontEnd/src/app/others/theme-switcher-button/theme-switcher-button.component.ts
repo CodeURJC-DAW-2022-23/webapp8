5d66faf717676b404929744f9047fdb140dffb6e
MiguelAngelSM <ma.sanchezm.2021@alumnos.urjc.es>
2023-04-21 23:28:07 +0200

    Fix bug
    Now the theme switcher is shown always

--- ProjectFrontEnd/src/app/others/theme-switcher-button/theme-switcher-button.component.ts
+++ ProjectFrontEnd/src/app/others/theme-switcher-button/theme-switcher-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 /**
  *  HOW TO USE THIS COMPONENT:
@@ -10,41 +10,24 @@ import { Component } from '@angular/core';
   templateUrl: './theme-switcher-button.component.html',
   styleUrls: ['./theme-switcher-button.component.css']
 })
-export class ThemeSwitcherButtonComponent {
+export class ThemeSwitcherButtonComponent implements OnInit{
+  dark:boolean;
   loadThemePage() {
-    var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-    var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
-
     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (
-      localStorage.getItem("color-theme") === "dark" ||
-      (!("color-theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
+    // Change the icons inside the button based on previous settings
+    this.dark = localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches);
+    if (this.dark) {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.remove("dark");
     }
-
-    // Change the icons inside the button based on previous settings
-    if (
-      localStorage.getItem("color-theme") === "dark" ||
-      (!("color-theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      themeToggleDarkIcon.classList.remove("hidden");
-    } else {
-      themeToggleLightIcon.classList.remove("hidden");
-    }
   };
 
   changeThemePage() {
-    var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-    var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
-
     // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle("hidden");
-    themeToggleLightIcon.classList.toggle("hidden");
+    this.dark = !this.dark;
 
     // if set via local storage previously
     if (localStorage.getItem("color-theme")) {
@@ -68,6 +51,9 @@ export class ThemeSwitcherButtonComponent {
     }
   }
 
+  ngOnInit():void{
+    this.loadThemePage();
+  }
 }
 
 
