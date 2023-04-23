57dc11f76ee8cd2af325333ff236576a1fc45849
Antonio José Alanís Bernal <antoniojalanis7131@gmail.com>
2023-04-24 00:44:01 +0200

    Add page titles

--- ProjectFrontEnd/src/app/app.component.ts
+++ ProjectFrontEnd/src/app/app.component.ts
@@ -6,13 +6,16 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  show:boolean;
-  constructor(private router: Router){}
+export class AppComponent implements OnInit {
+  public show: boolean;
+
+  constructor(private _router: Router){ }
+
   ngOnInit(): void {
       this.changePage();
   }
-  changePage(){
-    this.show = !(this.router.url.includes("error") || this.router.url.includes("login") || this.router.url.includes("register") || this.router.url === "/" || this.router.url.includes("verification") || this.router.url.includes("reset-password") || this.router.url.includes("forgot-password") || this.router.url.includes("write-tweet"))
+
+  public changePage(){
+      this.show = !(this._router.url.includes("error") || this._router.url.includes("login") || this._router.url.includes("register") || this._router.url === "/" || this._router.url.includes("verification") || this._router.url.includes("reset-password") || this._router.url.includes("forgot-password") || this._router.url.includes("write-tweet"))
   }
 }
