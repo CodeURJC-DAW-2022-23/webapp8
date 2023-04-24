c75ffcd7cc9fb5ae0063077ada4a34e192085bd4
Sergio de Oro Fernández <sergiodeoro422003@gmail.com>
2023-04-25 00:52:57 +0200

    Fix home charge bug

--- ProjectFrontEnd/src/app/screens/home/home.component.ts
+++ ProjectFrontEnd/src/app/screens/home/home.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  isCharged: boolean;
+
+  constructor(private userService: UserService){};
+  
+  ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(
+      response => this.isCharged = true
+    );
+  }
 }
