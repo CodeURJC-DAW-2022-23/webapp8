896a3ea263bbdd76e652febb3f3e4fa50f167373
Antonio José Alanís Bernal <antoniojalanis7131@gmail.com>
2023-04-24 21:27:59 +0200

    Upgrade Left bar responsive

--- ProjectFrontEnd/src/app/commons/left-bar/left-bar.component.ts
+++ ProjectFrontEnd/src/app/commons/left-bar/left-bar.component.ts
@@ -20,8 +20,8 @@ export class LeftBarComponent {
 
   public svg: string;
   private _userTypeSVGMap = {
-    "VERIFIED":"M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z",
-    "BANNED":"M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
+    "VERIFIED": "M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z",
+    "BANNED": "M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
   };
   public viewBox: string;
   private _viewBoxMap = {
@@ -32,7 +32,7 @@ export class LeftBarComponent {
   public isLogged: boolean;
   public navigation: string = '';
 
-  finish:boolean = false;
+  finish: boolean = false;
 
   constructor(private _userService: UserService,
     private _loginService: LoginService,
@@ -46,7 +46,7 @@ export class LeftBarComponent {
     this._userService.getCurrentUser().subscribe(userInformation => {
       this.isLogged = userInformation !== null;
       this.finish = true;
-      if(!this.isLogged){
+      if (!this.isLogged) {
         return
       }
       this.navigation = '/home'
@@ -56,7 +56,7 @@ export class LeftBarComponent {
       this.type = userInformation.user.type;
       this._loadSVG();
     },
-    error=>this.finish = true);
+      error => this.finish = true);
   }
 
   private _loadSVG(): void {
